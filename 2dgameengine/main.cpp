8cd437427df3dabffd43f683090166654df8fa13
Adriano <acsabo@hotmail.com>
2019-09-30 22:09:28 -0300

    Initial Commit

---
+++ 2dgameengine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2dgameengine
+//
+//  Created by Adriano Sabo on 30/09/19.
+//  Copyright © 2019 Adriano Sabo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
